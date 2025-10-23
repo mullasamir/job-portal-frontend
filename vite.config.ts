5cc42814a14176004f0e918d2aa2162f855cb4ac
thisisSAMIR7 <mullasamir799@.com>
2025-10-23 11:06:27 +0530

    temporary changes local

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default {
-  base: "/job-portal-frontend/",
-}
+export default defineConfig({
+  plugins: [react()],
+})
